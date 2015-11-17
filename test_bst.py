1c8dbc8b8bed92495b12caa422db87fa70fd8d46
Emad Mokhtar <emad.m.habib@gmail.com>
2015-11-17 13:41:36 +0300

    Add Test Find Method

--- test_bst.py
+++ test_bst.py
@@ -11,7 +11,7 @@ class TestBSTAppendMethod(unittest.TestCase):
         node1 = self.bt.find(10)
         self.assertEqual(node1.data, 10)
 
-    def test_append_two_node(self):
+    def test_append_two_nodes(self):
         self.bt.append(10)
         self.bt.append(2)
 
@@ -21,7 +21,7 @@ class TestBSTAppendMethod(unittest.TestCase):
         self.assertEqual(node1.data, 10)
         self.assertEqual(node2.data, 2)
 
-    def test_append_three_node(self):
+    def test_append_three_nodes(self):
         self.bt.append(10)
         self.bt.append(2)
         self.bt.append(3)
@@ -34,7 +34,7 @@ class TestBSTAppendMethod(unittest.TestCase):
         self.assertEqual(node2.data, 2)
         self.assertEqual(node3.data, 3)
 
-    def test_append_four_node(self):
+    def test_append_four_nodes(self):
         self.bt.append(10)
         self.bt.append(2)
         self.bt.append(3)
@@ -50,6 +50,59 @@ class TestBSTAppendMethod(unittest.TestCase):
         self.assertEqual(node3.data, 3)
         self.assertEqual(node4.data, 6)
 
+
+class TestBSTFindMethod(unittest.TestCase):
+
+    def setUp(self):
+        self.bt = bst.BinaryTree(5)
+
+    def test_find_one_node(self):
+        self.bt.append(10)
+
+        node1 = self.bt.find(10)
+
+        self.assertEqual(node1.data, 10)
+
+    def test_find_two_nodes(self):
+        self.bt.append(10)
+        self.bt.append(2)
+
+
+        node1 = self.bt.find(10)
+        node2 = self.bt.find(2)
+
+
+        self.assertEqual(node1.data, 10)
+        self.assertEqual(node2.data, 2)
+
+    def test_find_four_nodes(self):
+        self.bt.append(10)
+        self.bt.append(2)
+        self.bt.append(3)
+
+        node1 = self.bt.find(10)
+        node2 = self.bt.find(2)
+        node3 = self.bt.find(3)
+
+        self.assertEqual(node1.data, 10)
+        self.assertEqual(node2.data, 2)
+        self.assertEqual(node3.data, 3)
+
+    def test_find_four_nodes(self):
+        self.bt.append(10)
+        self.bt.append(2)
+        self.bt.append(3)
+        self.bt.append(6)
+
+        node1 = self.bt.find(10)
+        node2 = self.bt.find(2)
+        node3 = self.bt.find(3)
+        node4 = self.bt.find(6)
+
+        self.assertEqual(node1.data, 10)
+        self.assertEqual(node2.data, 2)
+        self.assertEqual(node3.data, 3)
+        self.assertEqual(node4.data, 6)
 
 if __name__ == '__main__':
     unittest.main()
