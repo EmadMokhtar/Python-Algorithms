c97c0927a33af17e14397077e1993f362b4c9ff1
Emad Mokhtar <emad.m.habib@gmail.com>
2015-11-19 17:29:24 +0300

    Add test for enhanced merge sort

--- Merge Sort/test_merge_sort.py
+++ Merge Sort/test_merge_sort.py
@@ -1,5 +1,5 @@
 import unittest
-import merge_sort_1
+import merge_sort
 
 class TestMergeSortStorageIssue(unittest.TestCase):
 
@@ -7,7 +7,7 @@ class TestMergeSortStorageIssue(unittest.TestCase):
         reverse_sorted = [9, 8, 7, 6, 5, 4, 3, 2, 1]
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        sorted_list = merge_sort_1.CopyMergeSort(reverse_sorted)
+        sorted_list = merge_sort.CopyMergeSort(reverse_sorted)
 
         self.assertEqual(expected, sorted_list)
 
@@ -16,7 +16,7 @@ class TestMergeSortStorageIssue(unittest.TestCase):
         sorted_list = [1, 2, 3, 3, 5, 6, 6, 8, 23, 34, 90]
         expected = [1, 2, 3, 3, 5, 6, 6, 8, 23, 34, 90]
 
-        sorted_list = merge_sort_1.CopyMergeSort(sorted_list)
+        sorted_list = merge_sort.CopyMergeSort(sorted_list)
 
         self.assertEqual(expected, sorted_list)
 
@@ -24,7 +24,7 @@ class TestMergeSortStorageIssue(unittest.TestCase):
         empty = []
         expected = []
 
-        sorted_list = merge_sort_1.CopyMergeSort(empty)
+        sorted_list = merge_sort.CopyMergeSort(empty)
 
         self.assertEqual(expected, sorted_list)
 
@@ -33,7 +33,7 @@ class TestMergeSortStorageIssue(unittest.TestCase):
         unsorted = [6, 23, 8, 34, 90, 1, 3, 5, 3, 2, 6]
         expected = [1, 2, 3, 3, 5, 6, 6, 8, 23, 34, 90]
 
-        sorted_list = merge_sort_1.CopyMergeSort(unsorted)
+        sorted_list = merge_sort.CopyMergeSort(unsorted)
 
         self.assertEqual(expected, sorted_list)
 
@@ -41,10 +41,54 @@ class TestMergeSortStorageIssue(unittest.TestCase):
         unsorted = [6, 23, 8, 34, 90, 1, 3, 23, 5, 3, 2, 6, 90]
         expected = [1, 2, 3, 3, 5, 6, 6, 8, 23, 23, 34, 90, 90]
 
-        sorted_list = merge_sort_1.CopyMergeSort(unsorted)
+        sorted_list = merge_sort.CopyMergeSort(unsorted)
 
         self.assertEqual(expected, sorted_list)
 
+
+class TestMergeSort(unittest.TestCase):
+
+    def test_sort_reverse_sorted_list(self):
+        reverse_sorted = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        merge_sort.MergeSort(reverse_sorted)
+
+        self.assertEqual(expected, reverse_sorted)
+
+
+    def test_sort_sorted_list(self):
+        sorted_list = [1, 2, 3, 3, 5, 6, 6, 8, 23, 34, 90]
+        expected = [1, 2, 3, 3, 5, 6, 6, 8, 23, 34, 90]
+
+        merge_sort.MergeSort(sorted_list)
+
+        self.assertEqual(expected, sorted_list)
+
+    def test_sort_empty_list(self):
+        empty = []
+        expected = []
+
+        merge_sort.MergeSort(empty)
+
+        self.assertEqual(expected, empty)
+
+
+    def test_sort_unsorted_list(self):
+        unsorted = [6, 23, 8, 34, 90, 1, 3, 5, 3, 2, 6]
+        expected = [1, 2, 3, 3, 5, 6, 6, 8, 23, 34, 90]
+
+        merge_sort.MergeSort(unsorted)
+
+        self.assertEqual(expected, unsorted)
+
+    def test_sort_list_has_duplicates(self):
+        unsorted = [6, 23, 8, 34, 90, 1, 3, 23, 5, 3, 2, 6, 90]
+        expected = [1, 2, 3, 3, 5, 6, 6, 8, 23, 23, 34, 90, 90]
+
+        merge_sort.MergeSort(unsorted)
+
+        self.assertEqual(expected, unsorted)
 
 if __name__ == '__main__':
     unittest.main()
